af4d90e59f75a1a270f81a579eac43f906763222
Tsveti Zlateva <tsveti.zlateva@gmail.com>
2020-03-18 11:31:57 +0200

    Explicit_Interfaces

---
+++ C# OOP/Explicit_Interfaces/Explicit_Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Explicit_Interfaces
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while (input.ToLower() != "end")
+            {
+                var tokens = input.Split(" ");
+                Citizen citizen = new Citizen(tokens[0], tokens[1], int.Parse(tokens[2]));
+                IPerson p = citizen;
+                Console.WriteLine(p.GetName()); 
+                IResident r = citizen;
+                Console.WriteLine(r.GetName()); 
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
